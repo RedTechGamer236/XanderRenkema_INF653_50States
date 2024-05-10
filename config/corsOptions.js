9e495fd100d9f541e68743cbb45ae5fc885447db
Xander Renkema <105459386+RedTechGamer236@users.noreply.github.com>
2024-05-09 20:21:00 -0500

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,17 +1,7 @@
-// Cross-Origin Resource Sharing
-const whiteList = [
-    "https://www.google.com",
-    "http://127.0.0.1:5500",
-    "http://localhost:3000",
-  ];
-  
-  const corsOptions = {
-    origin: (origin, callback) => {
-      if (whiteList.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    optionsSuccessStatus: 200,
-  };
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Allow all origins by passing null
+    callback(null, true);
+  },
+  optionsSuccessStatus: 200,
+};
